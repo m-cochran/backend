1494593b8ec9d1ceb460defcc9137e2b99959a17
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-09-06 07:06:12 -0400

    Create create-payment-intent.js

---
+++ vercel-stripe-backend/api/create-payment-intent.js
@@ -0,0 +1,25 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Use environment variable for security
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests allowed' });
+  }
+
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount, // Amount in cents
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    res.status(200).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+};
