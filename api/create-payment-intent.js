31027f4a84f6ed6d0cabbf654b9b398492ec0f3b
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-09-10 00:21:39 -0400

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const cors = require('cors');
-const app = express();
+
 
 app.use(express.json());
 
