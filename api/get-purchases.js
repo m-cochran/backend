84a540a269b9f08f85c1a48e05786466f6ab5db4
m-cochran <106445623+m-cochran@users.noreply.github.com>
2025-01-01 16:30:32 -0500

    Delete api/get-purchases.js

--- api/get-purchases.js
+++
@@ -1,6 +0,0 @@
-app.get('/api/get-purchases', async (req, res) => {
-  const { email } = req.query;
-
-  const purchases = await Purchase.find({ email }).sort({ date: -1 }).limit(5);
-  res.json(purchases);
-});
