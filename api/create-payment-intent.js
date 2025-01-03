d544ff0eb5fe3dce83a2e86f27eec37cd9e4a4db
m-cochran <106445623+m-cochran@users.noreply.github.com>
2025-01-02 20:19:31 -0500

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,5 +1,17 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+export default async function handler(req, res) {
+  // Handle CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');  // For allowing all domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');  // Allow relevant methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow specific headers
+
+  if (req.method === 'OPTIONS') {
+    // Handle preflight request
+    return res.status(200).end();
+  }
+
+
 module.exports = async (req, res) => {
   if (req.method === "POST") {
     const { amount, email, name, phone, address } = req.body;
